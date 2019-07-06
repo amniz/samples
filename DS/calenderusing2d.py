be68a627a6a58ebe081c4dc8d686e23f3bcd9938
amniz <iamnzm@outlook.com>
2019-07-06 19:20:00 +0530

    done with calender month printing pending

--- DS/calenderusing2d.py
+++ DS/calenderusing2d.py
@@ -38,27 +38,47 @@ m=int(sys.argv[1])
 y=int(sys.argv[2])
 k=daysofWeek(m,y)
 d=int(k)
+go=d+9
 days=[x for x in range(1,monthDay(m,y))]
-print("days",days[0])
-print("S M T W Th F S")
+print("     python cal",m," ",y)
+print("    S   M  T   W  Th   F   S")
 li=[]
 val=42
+c=0
 while val>0:
     g=[]
     n=0
-    c=1
+    
     while n<=6:
         while d>0:
             g.append(" ")
             n+=1
             val-=1
             d-=1
-        g.append(c)
-        c+=1
-        n+=1
-        val-=1
-    li.extend(g)
-print(li)
+        try:
+            g.append(days[c])
+            c+=1
+            n+=1
+            val-=1
+        except:
+            g.append(" ")
+            c+=1
+            n+=1
+            val-=1
+    li.append(g)
+
+for k in li:
+    
+    n=0
+    while n<=6:
+        if go>0:
+            print("  ",k[n],end="")
+            n+=1
+            go-=1
+        else:
+            print(" ",k[n],end="")
+            n+=1
+    print()
         
         
     
