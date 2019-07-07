24d4d5f41f595636c460459232f9efc6089a3c7a
amniz <iamnzm@outlook.com>
2019-07-07 15:03:25 +0530

    check and verify

---
+++ DS/ds13.py
@@ -0,0 +1,43 @@
+def isPrime():
+        l=[]
+        for i in range(10,1000):
+           if i==2:
+                l.append(i)
+           else:
+                j=2
+                while(j<=i):
+                    if i%j==0:
+                        break
+                       
+                    else:
+                          j+=1
+
+
+                if j==i:
+                    l.append(i)
+        return l
+l=isPrime()
+
+from Linkedlist import Linkedlist
+def checkana(l):
+    ana=set()
+    cou=0
+    for i in l:
+        j=str(i)
+        k=sorted(j)
+        for j in [x for x in l[cou+1:len(l)]]:
+            h=str(j)
+            m=sorted(h)
+            if k==m:
+                ana.add(i)
+                ana.add(j)
+        cou+=1
+    return ana
+pa=checkana(l)
+j=sorted(pa)
+print(j)
+lo=Linkedlist()
+for i in j:
+    lo.add(i)
+lo.display()
+
