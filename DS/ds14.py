a1350285dab7a94600aa7e9546e7cfb91791e667
amniz <iamnzm@outlook.com>
2019-07-07 15:03:52 +0530

    checked and verified ds14

---
+++ DS/ds14.py
@@ -0,0 +1,42 @@
+from RealQueue import Queue
+def isPrime():
+        l=[]
+        for i in range(10,1000):
+           if i==2:
+                l.append(i)
+           else:
+                j=2
+                while(j<=i):
+                    if i%j==0:
+                        break
+                       
+                    else:
+                          j+=1
+
+
+                if j==i:
+                    l.append(i)
+        return l
+l=isPrime()
+
+def checkana(l):
+    ana=set()
+    cou=0
+    for i in l:
+        j=str(i)
+        k=sorted(j)
+        for j in [x for x in l[cou+1:len(l)]]:
+            h=str(j)
+            m=sorted(h)
+            if k==m:
+                ana.add(i)
+                ana.add(j)
+        cou+=1
+    return ana
+q=Queue()
+anag=checkana(l)
+g=sorted(anag)
+for i in g:
+    #print(i)
+    Queue.add(q,i)
+Queue.display(q)
