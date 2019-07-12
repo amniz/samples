ca025c96fd8f8c24b0b196b83442e8d5845147a1
nisam <iamnzm@outlook.com>
2019-07-12 17:24:23 +0530

    added comments for algorithm

--- Algorithms/Algo1.py
+++ Algorithms/Algo1.py
@@ -2,4 +2,6 @@
 #@version:3.7
 #@purpose: Anagram Detection
 from util import util
-util.anagram()#checking for anagram
+str1=input("enter string 1")#getting the input from the user
+str2=input("enter string 2")#getting the input from the user
+util.anagram(str1,str2)#checking for anagram
